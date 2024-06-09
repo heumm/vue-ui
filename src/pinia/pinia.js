4d0908b2909b7f329d58d77475830e746c475f5c
hanheum <hanheum97@gmail.com>
2024-06-09 23:12:02 +0900

    add localstorage state manage plugin

---
+++ src/pinia/pinia.js
@@ -0,0 +1,23 @@
+import { createPinia } from 'pinia';
+
+// 플러그인 함수 정의
+const usePersistedStatePlugin = ({ store }) => {
+	// 스토어의 상태를 localStorage에서 복원
+	const storedState = localStorage.getItem(store.$id);
+	if (storedState) {
+		store.$patch(JSON.parse(storedState));
+	}
+
+	// 스토어의 상태가 변경될 때마다 localStorage에 저장
+	store.$subscribe((mutation, state) => {
+		localStorage.setItem(store.$id, JSON.stringify(state));
+	});
+};
+
+// Pinia 인스턴스 생성
+const pinia = createPinia();
+
+// 플러그인 등록
+pinia.use(usePersistedStatePlugin);
+
+export default pinia;
