d3723981746e3a264878de8ccb1d95c483eb3c37
hanheum <hanheum97@gmail.com>
2024-06-09 23:16:18 +0900

    update

--- src/stores/store.js
+++ src/stores/store.js
@@ -1,81 +1,38 @@
 import { ref, computed } from 'vue';
 import { defineStore } from 'pinia';
-
-const menu = [
-	{
-		id: 1,
-		name: '교회소식',
-		uri: '/detail/posts',
-		children: []
-	},
-	{
-		id: 2,
-		name: '예배말씀',
-		uri: '/detail/verses',
-		children: [
-			{
-				id: 1,
-				name: '오늘의 묵상',
-				uri: '/detail/todayqt'
-			},
-			{
-				id: 2,
-				name: '주보',
-				uri: '/detail/bulletin'
-			},
-			{
-				id: 2,
-				name: '메뉴2',
-				uri: '/detail/bulletin'
-			},
-			{
-				id: 2,
-				name: '메뉴3',
-				uri: '/detail/bulletin'
-			},
-			{
-				id: 2,
-				name: '메뉴4',
-				uri: '/detail/bulletin'
-			},
-			{
-				id: 2,
-				name: '메뉴5',
-				uri: '/detail/bulletin'
-			},
-			{
-				id: 2,
-				name: '메뉴6',
-				uri: '/detail/bulletin'
-			}
-		]
-	}
-];
+import axios from '@/axios/axios.js';
 
 export const useMemberStore = defineStore('member', () => {
 	const id = ref();
 	return { id };
 });
 
-export const useMenuStore = defineStore('menues', () => {
-	const menuList = menu;
+export const useMenuStore = defineStore('menus', () => {
+	const menuList = ref([]);
 	const getMenus = computed(() => {
-		return menuList;
+		return menuList.value;
 	});
 	const selectedMenuId = ref(0);
 	const selectedMenu = computed(() => {
-		for (const menu of menuList) {
+		for (const menu of menuList.value) {
 			if (menu.id === selectedMenuId.value) {
-				localStorage.setItem('selectedMenu', JSON.stringify(menu));
-
+				// localStorage.setItem('selectedMenu', JSON.stringify(menu));
+				// console.log(JSON.parse(localStorage.getItem('selectedMenu')));
 				return menu;
 			} else if (selectedMenuId.value === 0) {
-				return JSON.parse(localStorage.getItem('selectedMenu'));
+				// return JSON.parse(localStorage.getItem('selectedMenu'));
 			}
 		}
 	});
-
-	return { selectedMenuId, getMenus, selectedMenu }; //getMenus를 포함한 객체를 반환하여 store를 정의한다.
+	const fetchMenus = async () => {
+		try {
+			const res = await axios.get('/api/v1/menu');
+			menuList.value = res.data;
+		} catch (err) {
+			console.error('Failed to fetch menus:', err);
+		}
+	};
+	return { selectedMenuId, getMenus, selectedMenu, fetchMenus }; //getMenus를 포함한 객체를 반환하여 store를 정의한다.
 });
 
 export const useBoardLayoutStore = defineStore('boardLayouts', () => {
