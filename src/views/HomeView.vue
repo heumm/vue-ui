db6ca6ddd081948549819924d24c4a07eb3097bc
hanheum <hanheum97@gmail.com>
2024-08-11 22:23:33 +0900

    carousel이미지 회전 컴포넌트 추가

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,27 @@
 <template>
 	<div>
-		<div class="col-span-full min-h-dvh">메인 페이지입니다.</div>
+		<div class="col-span-full min-h-dvh">
+			<sm-carousel :images="images"></sm-carousel>
+		</div>
 	</div>
 </template>
 
 <script setup>
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
+import SmCarousel from '@/components/SmCarousel.vue';
+
+const images = ref([
+	'https://via.placeholder.com/800x400?text=Slide+1',
+	'https://via.placeholder.com/800x400?text=Slide+2',
+	'https://via.placeholder.com/800x400?text=Slide+3'
+]);
 
 const route = useRoute();
 const router = useRouter();
 // onMounted(() => {
 if (route.query.back) {
-	router.go(-1);
+	// router.go(-1);
 }
 // });
 </script>
